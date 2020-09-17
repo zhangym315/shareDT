48cc0d34cade33525b80ff7e252b8effc4be65ad
YIMING ZHANG <32471860+zhangym315@users.noreply.github.com>
2020-09-17 21:25:14 +1000

    nit

--- src/MainService.cpp
+++ src/MainService.cpp
@@ -212,7 +212,7 @@ void HandleCommandSocket(int fd, char * buf)
                                 NULL,    // Process handle not inheritable
                                 NULL,    // Thread handle not inheritable
                                 true,    // Set handle inheritance to FALSE
-                                CREATE_NO_WINDOW,       // No creation flags
+                                CREATE_NO_WINDOW, // no console window
                                 NULL,    // Use parent's environment block
                                 NULL,    // Use parent's starting directory
                                 &si,     // Pointer to STARTUPINFO structure
