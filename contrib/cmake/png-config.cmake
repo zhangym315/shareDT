9a8bf16549d67dd7eb15f13af26c0610cb390b7b
YIMING ZHANG <32471860+zhangym315@users.noreply.github.com>
2020-12-05 22:11:42 +1100

    update

--- contrib/cmake/png-config.cmake
+++ contrib/cmake/png-config.cmake
@@ -2,7 +2,7 @@
 set(PNG_INSTALL ${CMAKE_SOURCE_CONTRIB}/libpng-1.6.37/build/install/)
 set(PNG_INCLUDE_DIR ${PNG_INSTALL}/include/)
 if(WIN32)
-    set(PNG_LIBRARIES ${PNG_INSTALL}/lib/libpng16_staticd.lib)
+    set(PNG_LIBRARIES ${PNG_INSTALL}/lib/libpng16_static.lib)
 else()
     set(PNG_LIBRARIES ${PNG_INSTALL}/lib/libpng.a)
 endif()
