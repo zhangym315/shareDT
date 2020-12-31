1d97ccda38d9e7606b96010c4ae47fc8e3eaa99e
yimingz <yimingz@splunk.com>
2020-12-31 21:59:23 +1100

    add contrib/cmake/x265-config.cmake

---
+++ contrib/cmake/x265-config.cmake
@@ -0,0 +1,9 @@
+
+set(X265_INSTALL ${CMAKE_SOURCE_CONTRIB}/x265_3.3/source/build/install/)
+set(X265_INCLUDE_DIR ${X265_INSTALL}/include/)
+
+set(X265_LIBRARIES ${X265_INSTALL}/lib/libx265.a)
+set(X265_LIBRARY ${X265_LIBRARIES})
+
+message(STATUS "Found X265 at ${X265_INSTALL}")
+set(X265_FOUND TRUE)
